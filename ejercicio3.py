2fa40f7342ace346719886dd176ce999b02954e9
unknown <ciro.arreguin05@gmail.com>
2024-04-11 23:15:43 -0600

    Ejercicio 3 funcional

--- ejercicio3.py
+++ ejercicio3.py
@@ -18,5 +18,9 @@ def Ope (num1, num2):
 
 resp = int(input("Cual es la operacion que quieres realizar? No.1 suma, No.2 resta, No.3 multiplicación, No.4 Division "))
 
+num1 = int(input("Ingresa el primer numero: "))
+num2 = int(input("Ingresa el segundo numero: "))
 
+Rs_ope = Ope (num1, num2)
+print(f"El resultado de la operacion es:  {Rs_ope}")
         
