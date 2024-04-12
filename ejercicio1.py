b41c3502a97a66e1b717bf9261ed2dc73619a821
unknown <ciro.arreguin05@gmail.com>
2024-04-11 22:28:40 -0600

    Prueba ejercicio2

--- ejercicio1.py
+++ ejercicio1.py
@@ -3,3 +3,8 @@ def tabla (numero):
         print (f"{numero}X{x} = {numero*x}")
 
 tabla(int(input("Cual es la tabla que quieres: ")))
+
+
+def area (lado):
+    int(input("Cuanto es el lado del cuadrado: "))
+    print (f"El area del cuadrado es: {lado*lado}")
